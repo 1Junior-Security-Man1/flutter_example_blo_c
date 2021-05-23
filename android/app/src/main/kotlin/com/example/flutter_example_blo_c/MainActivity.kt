29dce38f571c43aa5de6bedeb69cc8c88860cf07
Alex <sasha228023@gmail.com>
2021-05-23 16:03:35 +0300

    Initial commit (bloc_test)

---
+++ android/app/src/main/kotlin/com/example/flutter_example_blo_c/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_example_blo_c
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_example_blo_c/view/posts_page.dart';
+
+
+class App extends MaterialApp {
+  App() : super(home: PostsPage());
+}
